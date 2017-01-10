08cf36cfa4cf679009f04d1d76a9a2836ec6bbac
tarcisomesquita <tarcisomesquita@gmail.com>
2017-01-10 17:55:06 -0200

    2017-01-10

--- README.md
+++ README.md
@@ -1,5 +1,45 @@
-# nix
-Validação de sequências de palavras
+Nix
+===
 
-Isso é apenas uma idéia por enquanto.
+Projeto: Validação de sequências de palavras.
+--------
+
+Problema: Hoje, se alguém digitar as informações "elefante é uma ave", "carro tem asa", "número atômico do carbono é 20", o editor de texto não diz que elas estão erradas.
+---------
+
+Objetivo: Validar sequência de palavras usando base de conhecimento.
+---------
+
+Exemplo de base de conhecimento:
+
+&lt;<http://dbpedia.org/resource/Aedes_aegypti>&gt; &lt;<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>&gt; &lt;<http://dbpedia.org/ontology/Insect>&gt; .
+&lt;<http://dbpedia.org/resource/Aedes_aegypti>&gt; &lt;<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>&gt; &lt;<http://dbpedia.org/ontology/Animal>&gt; .
+&lt;<http://dbpedia.org/ontology/Insect>&gt; &lt;<http://www.w3.org/2000/01/rdf-schema#label>&gt; "inseto"@pt .
+
+
+Exemplos de texto validado:
+
+`Aedes aegypti é um animal` que possui cabeça, tórax e abdômen, como qualquer inseto.
+
+O `inseto aedes aegypty` é o vetor transmissor do vírus da dengue.
+
+Ideia 1
+
+Extrair triplas RDF de textos usando Processamento de Linguagem Natural e compará-las com uma base que já existia.
+
+Acho esse método complexo demais.
+Ideia 2
+
+Usar SPARQL query para obter o resource que contem o label igual à palavra do texto.
+
+Devido à dificuldade com polissemia, não marcar apenas uma palavra.
+
+Dificuldade com HTTP header Accept. Teste usando iframe, form, script. Talvez usando AJAX.
+
+Dificuldade com a organização das bases disponíveis na internet.
+
+
+Autor: [Tarciso].
+
+[Tarciso]: https://tarcisomesquita.blogspot.com.br
 
--- base/RDF/serilisations/nt_to_ttl.sh
+++ base/RDF/serilisations/nt_to_ttl.sh
@@ -12,147 +12,15 @@ if [ -f "$OUTPUT" ] ; then
    exit 1
 fi
 
-# cat nameSpace.txt | cut -d' ' -f2-3 | sed 's/\//\\\//g; s/\([^ ]*\) <\([^>]*\)>/   s\/<\2\\([^>]*\\)>\/\1\\1\/g\;/g;'
+if [ -f "$(dirname $OUTPUT)/rascunho1" ] ; then
+   echo "Já existe um arquivo chamado \"$(dirname $OUTPUT)/rascunho1\""
+   exit 1
+fi
 
-echo '
-@prefix a: <http://www.w3.org/2005/Atom> .
-@prefix address: <http://schemas.talis.com/2005/address/schema#> .
-@prefix admin: <http://webns.net/mvcb/> .
-@prefix atom: <http://atomowl.org/ontologies/atomrdf#> .
-@prefix aws: <http://soap.amazon.com/> .
-@prefix b3s: <http://b3s.openlinksw.com/> .
-@prefix batch: <http://schemas.google.com/gdata/batch> .
-@prefix bibo: <http://purl.org/ontology/bibo/> .
-@prefix bugzilla: <http://www.openlinksw.com/schemas/bugzilla#> .
-@prefix c: <http://www.w3.org/2002/12/cal/icaltzd#> .
-@prefix campsite: <http://www.openlinksw.com/campsites/schema#> .
-@prefix cb: <http://www.crunchbase.com/> .
-@prefix cc: <http://web.resource.org/cc/> .
-@prefix content: <http://purl.org/rss/1.0/modules/content/> .
-@prefix cv: <http://purl.org/captsolo/resume-rdf/0.2/cv#> .
-@prefix cvbase: <http://purl.org/captsolo/resume-rdf/0.2/base#> .
-@prefix dawgt: <http://www.w3.org/2001/sw/DataAccess/tests/test-dawg#> .
-@prefix dbc: <http://dbpedia.org/resource/Category:> .
-@prefix dbo: <http://dbpedia.org/ontology/> .
-@prefix dbp: <http://dbpedia.org/property/> .
-@prefix dbr: <http://dbpedia.org/resource/> .
-@prefix dc: <http://purl.org/dc/elements/1.1/> .
-@prefix dct: <http://purl.org/dc/terms/> .
-@prefix digg: <http://digg.com/docs/diggrss/> .
-@prefix dul: <http://www.ontologydesignpatterns.org/ont/dul/DUL.owl> .
-@prefix enc: <http://purl.oclc.org/net/rss_2.0/enc#> .
-@prefix exif: <http://www.w3.org/2003/12/exif/ns/> .
-@prefix fb: <http://api.facebook.com/1.0/> .
-@prefix ff: <http://api.friendfeed.com/2008/03> .
-@prefix fn: <http://www.w3.org/2005/xpath-functions/#> .
-@prefix foaf: <http://xmlns.com/foaf/0.1/> .
-@prefix freebase: <http://rdf.freebase.com/ns/> .
-@prefix g: <http://base.google.com/ns/1.0> .
-@prefix gb: <http://www.openlinksw.com/schemas/google-base#> .
-@prefix gd: <http://schemas.google.com/g/2005> .
-@prefix geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> .
-@prefix geonames: <http://www.geonames.org/ontology#> .
-@prefix georss: <http://www.georss.org/georss/> .
-@prefix gml: <http://www.opengis.net/gml> .
-@prefix go: <http://purl.org/obo/owl/GO#> .
-@prefix hlisting: <http://www.openlinksw.com/schemas/hlisting/> .
-@prefix hoovers: <http://wwww.hoovers.com/> .
-@prefix hrev: <http://www.purl.org/stuff/hrev#> .
-@prefix ical: <http://www.w3.org/2002/12/cal/ical#> .
-@prefix ir: <http://web-semantics.org/ns/image-regions> .
-@prefix itunes: <http://www.itunes.com/DTDs/Podcast-1.0.dtd> .
-@prefix ldp: <http://www.w3.org/ns/ldp#> .
-@prefix lgv: <http://linkedgeodata.org/vocabulary#> .
-@prefix link: <http://www.xbrl.org/2003/linkbase> .
-@prefix lod: <http://lod.openlinksw.com/> .
-@prefix math: <http://www.w3.org/2000/10/swap/math#> .
-@prefix media: <http://search.yahoo.com/mrss/> .
-@prefix mesh: <http://purl.org/commons/record/mesh/> .
-@prefix mf: <http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#> .
-@prefix mmd: <http://musicbrainz.org/ns/mmd-1.0#> .
-@prefix mo: <http://purl.org/ontology/mo/> .
-@prefix mql: <http://www.freebase.com/> .
-@prefix nci: <http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#> .
-@prefix nfo: <http://www.semanticdesktop.org/ontologies/nfo/#> .
-@prefix ng: <http://www.openlinksw.com/schemas/ning#> .
-@prefix nyt: <http://www.nytimes.com/> .
-@prefix oai: <http://www.openarchives.org/OAI/2.0/> .
-@prefix oai_dc: <http://www.openarchives.org/OAI/2.0/oai_dc/> .
-@prefix obo: <http://www.geneontology.org/formats/oboInOwl#> .
-@prefix ogc: <http://www.opengis.net/> .
-@prefix ogcgml: <http://www.opengis.net/ont/gml#> .
-@prefix ogcgs: <http://www.opengis.net/ont/geosparql#> .
-@prefix ogcgsf: <http://www.opengis.net/def/function/geosparql/> .
-@prefix ogcgsr: <http://www.opengis.net/def/rule/geosparql/> .
-@prefix ogcsf: <http://www.opengis.net/ont/sf#> .
-@prefix openSearch: <http://a9.com/-/spec/opensearchrss/1.0/> .
-@prefix opencyc: <http://sw.opencyc.org/2008/06/10/concept/> .
-@prefix opl: <http://www.openlinksw.com/schema/attribution#> .
-@prefix opl-gs: <http://www.openlinksw.com/schemas/getsatisfaction/> .
-@prefix opl-meetup: <http://www.openlinksw.com/schemas/meetup/> .
-@prefix opl-xbrl: <http://www.openlinksw.com/schemas/xbrl/> .
-@prefix oplweb: <http://www.openlinksw.com/schemas/oplweb#> .
-@prefix ore: <http://www.openarchives.org/ore/terms/> .
-@prefix owl: <http://www.w3.org/2002/07/owl#> .
-@prefix product: <http://www.buy.com/rss/module/productV2/> .
-@prefix protseq: <http://purl.org/science/protein/bysequence/> .
-@prefix r: <http://backend.userland.com/rss2> .
-@prefix radio: <http://www.radiopop.co.uk/> .
-@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-@prefix rdfa: <http://www.w3.org/ns/rdfa#> .
-@prefix rdfdf: <http://www.openlinksw.com/virtrdf-data-formats#> .
-@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-@prefix rev: <http://purl.org/stuff/rev#> .
-@prefix review: <http://www.purl.org/stuff/rev#> .
-@prefix rss: <http://purl.org/rss/1.0/> .
-@prefix sc: <http://purl.org/science/owl/sciencecommons/> .
-@prefix scovo: <http://purl.org/NET/scovo#> .
-@prefix sd: <http://www.w3.org/ns/sparql-service-description#> .
-@prefix sioc: <http://rdfs.org/sioc/ns#> .
-@prefix sioct: <http://rdfs.org/sioc/types#> .
-@prefix skiresort: <http://www.openlinksw.com/ski_resorts/schema#> .
-@prefix skos: <http://www.w3.org/2004/02/skos/core#> .
-@prefix slash: <http://purl.org/rss/1.0/modules/slash/> .
-@prefix stock: <http://xbrlontology.com/ontology/finance/stock_market#> .
-@prefix twfy: <http://www.openlinksw.com/schemas/twfy#> .
-@prefix umbel: <http://umbel.org/umbel#> .
-@prefix umbel-ac: <http://umbel.org/umbel/ac/> .
-@prefix umbel-rc: <http://umbel.org/umbel/rc/> .
-@prefix umbel-sc: <http://umbel.org/umbel/sc/> .
-@prefix uniprot: <http://purl.uniprot.org/> .
-@prefix units: <http://dbpedia.org/units/> .
-@prefix usc: <http://www.rdfabout.com/rdf/schema/uscensus/details/100pct/> .
-@prefix v: <http://www.openlinksw.com/xsltext/> .
-@prefix vcard: <http://www.w3.org/2001/vcard-rdf/3.0#> .
-@prefix vcard2006: <http://www.w3.org/2006/vcard/ns#> .
-@prefix vi: <http://www.openlinksw.com/virtuoso/xslt/> .
-@prefix virt: <http://www.openlinksw.com/virtuoso/xslt> .
-@prefix virtcxml: <http://www.openlinksw.com/schemas/virtcxml#> .
-@prefix virtpivot: <http://www.openlinksw.com/schemas/virtpivot#> .
-@prefix virtrdf: <http://www.openlinksw.com/schemas/virtrdf#> .
-@prefix void: <http://rdfs.org/ns/void#> .
-@prefix wb: <http://www.worldbank.org/> .
-@prefix wdrs: <http://www.w3.org/2007/05/powder-s#> .
-@prefix wf: <http://www.w3.org/2005/01/wf/flow#> .
-@prefix wfw: <http://wellformedweb.org/CommentAPI/> .
-@prefix wikicompany: <http://dbpedia.openlinksw.com/wikicompany/> .
-@prefix wikidata: <http://www.wikidata.org/entity/> .
-@prefix xf: <http://www.w3.org/2004/07/xpath-functions> .
-@prefix xfn: <http://gmpg.org/xfn/11#> .
-@prefix xhtml: <http://www.w3.org/1999/xhtml> .
-@prefix xhv: <http://www.w3.org/1999/xhtml/vocab#> .
-@prefix xi: <http://www.xbrl.org/2003/instance> .
-@prefix xml: <http://www.w3.org/XML/1998/namespace> .
-@prefix xn: <http://www.ning.com/atom/1.0> .
-@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
-@prefix xsl10: <http://www.w3.org/XSL/Transform/1.0> .
-@prefix xsl1999: <http://www.w3.org/1999/XSL/Transform> .
-@prefix xslwd: <http://www.w3.org/TR/WD-xsl> .
-@prefix yago: <http://dbpedia.org/class/yago/> .
-@prefix yago-res: <http://mpii.de/yago/resource/> .
-@prefix yt: <http://gdata.youtube.com/schemas/2007> .
-@prefix zem: <http://s.zemanta.com/ns#> .
-' > "$OUTPUT"
+if [ -f "$(dirname $OUTPUT)/rascunho2" ] ; then
+   echo "Já existe um arquivo chamado \"$(dirname $OUTPUT)/rascunho2\""
+   exit 1
+fi
 
 sed -e '
    s/<http:\/\/www.w3.org\/2005\/Atom\([^>]*\)>/a:\1/g;
@@ -292,7 +160,192 @@ sed -e '
    s/<http:\/\/mpii.de\/yago\/resource\/\([^>]*\)>/yago-res:\1/g;
    s/<http:\/\/gdata.youtube.com\/schemas\/2007\([^>]*\)>/yt:\1/g;
    s/<http:\/\/s.zemanta.com\/ns#\([^>]*\)>/zem:\1/g;
-' "$1" >> "$OUTPUT"
+' "$1" >> "$(dirname $OUTPUT)/rascunho1"
+
+sed -i -e 's/^\(.*\)$/ \1/g;' "$(dirname $OUTPUT)/rascunho1"
+
+
+# cat nameSpace.txt | cut -d' ' -f2-3 | sed 's/\//\\\//g; s/\([^ ]*\) <\([^>]*\)>/   s\/<\2\\([^>]*\\)>\/\1\\1\/g\;/g;'
+
+if grep -q " rdf:" "$(dirname $OUTPUT)/rascunho1" ; then
+   echo "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> ." >> "$(dirname $OUTPUT)/rascunho2";
+fi
+
+if grep -q " rdfs:" "$(dirname $OUTPUT)/rascunho1" ; then
+   echo "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> ." >> "$(dirname $OUTPUT)/rascunho2";
+fi
+
+if grep -q " dbc:" "$(dirname $OUTPUT)/rascunho1" ; then
+   echo "@prefix dbc: <http://dbpedia.org/resource/Category:> ." >> "$(dirname $OUTPUT)/rascunho2";
+fi
+
+if grep -q " dbo:" "$(dirname $OUTPUT)/rascunho1" ; then
+   echo "@prefix dbo: <http://dbpedia.org/ontology/> ." >> "$(dirname $OUTPUT)/rascunho2";
+fi
+
+if grep -q " dbp:" "$(dirname $OUTPUT)/rascunho1" ; then
+   echo "@prefix dbp: <http://dbpedia.org/property/> ." >> "$(dirname $OUTPUT)/rascunho2";
+fi
+
+if grep -q " dbr:" "$(dirname $OUTPUT)/rascunho1" ; then
+   echo "@prefix dbr: <http://dbpedia.org/resource/> ." >> "$(dirname $OUTPUT)/rascunho2";
+fi
+
+if grep -q " dc:" "$(dirname $OUTPUT)/rascunho1" ; then
+   echo "@prefix dc: <http://purl.org/dc/elements/1.1/> ." >> "$(dirname $OUTPUT)/rascunho2";
+fi
+
+if grep -q " foaf:" "$(dirname $OUTPUT)/rascunho1" ; then
+   echo "@prefix foaf: <http://xmlns.com/foaf/0.1/> ." >> "$(dirname $OUTPUT)/rascunho2";
+fi
+
+if grep -q " owl:" "$(dirname $OUTPUT)/rascunho1" ; then
+   echo "@prefix owl: <http://www.w3.org/2002/07/owl#> ." >> "$(dirname $OUTPUT)/rascunho2";
+fi
+
+if grep -q "\^\^xsd:" "$(dirname $OUTPUT)/rascunho1" || grep -q " xsd:" "$(dirname $OUTPUT)/rascunho1" ; then
+   echo "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> ." >> "$(dirname $OUTPUT)/rascunho2";
+fi
+
+if grep -q " obo:" "$(dirname $OUTPUT)/rascunho1" ; then
+   echo "@prefix obo: <http://www.geneontology.org/formats/oboInOwl#> ." >> "$(dirname $OUTPUT)/rascunho2";
+fi
+
+echo '
+@prefix rdfa: <http://www.w3.org/ns/rdfa#> .
+@prefix rdfdf: <http://www.openlinksw.com/virtrdf-data-formats#> .
+
+@prefix a: <http://www.w3.org/2005/Atom> .
+@prefix address: <http://schemas.talis.com/2005/address/schema#> .
+@prefix admin: <http://webns.net/mvcb/> .
+@prefix atom: <http://atomowl.org/ontologies/atomrdf#> .
+@prefix aws: <http://soap.amazon.com/> .
+@prefix b3s: <http://b3s.openlinksw.com/> .
+@prefix batch: <http://schemas.google.com/gdata/batch> .
+@prefix bibo: <http://purl.org/ontology/bibo/> .
+@prefix bugzilla: <http://www.openlinksw.com/schemas/bugzilla#> .
+@prefix c: <http://www.w3.org/2002/12/cal/icaltzd#> .
+@prefix campsite: <http://www.openlinksw.com/campsites/schema#> .
+@prefix cb: <http://www.crunchbase.com/> .
+@prefix cc: <http://web.resource.org/cc/> .
+@prefix content: <http://purl.org/rss/1.0/modules/content/> .
+@prefix cv: <http://purl.org/captsolo/resume-rdf/0.2/cv#> .
+@prefix cvbase: <http://purl.org/captsolo/resume-rdf/0.2/base#> .
+@prefix dawgt: <http://www.w3.org/2001/sw/DataAccess/tests/test-dawg#> .
+@prefix dct: <http://purl.org/dc/terms/> .
+@prefix digg: <http://digg.com/docs/diggrss/> .
+@prefix dul: <http://www.ontologydesignpatterns.org/ont/dul/DUL.owl> .
+@prefix enc: <http://purl.oclc.org/net/rss_2.0/enc#> .
+@prefix exif: <http://www.w3.org/2003/12/exif/ns/> .
+@prefix fb: <http://api.facebook.com/1.0/> .
+@prefix ff: <http://api.friendfeed.com/2008/03> .
+@prefix fn: <http://www.w3.org/2005/xpath-functions/#> .
+@prefix freebase: <http://rdf.freebase.com/ns/> .
+@prefix g: <http://base.google.com/ns/1.0> .
+@prefix gb: <http://www.openlinksw.com/schemas/google-base#> .
+@prefix gd: <http://schemas.google.com/g/2005> .
+@prefix geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> .
+@prefix geonames: <http://www.geonames.org/ontology#> .
+@prefix georss: <http://www.georss.org/georss/> .
+@prefix gml: <http://www.opengis.net/gml> .
+@prefix go: <http://purl.org/obo/owl/GO#> .
+@prefix hlisting: <http://www.openlinksw.com/schemas/hlisting/> .
+@prefix hoovers: <http://wwww.hoovers.com/> .
+@prefix hrev: <http://www.purl.org/stuff/hrev#> .
+@prefix ical: <http://www.w3.org/2002/12/cal/ical#> .
+@prefix ir: <http://web-semantics.org/ns/image-regions> .
+@prefix itunes: <http://www.itunes.com/DTDs/Podcast-1.0.dtd> .
+@prefix ldp: <http://www.w3.org/ns/ldp#> .
+@prefix lgv: <http://linkedgeodata.org/vocabulary#> .
+@prefix link: <http://www.xbrl.org/2003/linkbase> .
+@prefix lod: <http://lod.openlinksw.com/> .
+@prefix math: <http://www.w3.org/2000/10/swap/math#> .
+@prefix media: <http://search.yahoo.com/mrss/> .
+@prefix mesh: <http://purl.org/commons/record/mesh/> .
+@prefix mf: <http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#> .
+@prefix mmd: <http://musicbrainz.org/ns/mmd-1.0#> .
+@prefix mo: <http://purl.org/ontology/mo/> .
+@prefix mql: <http://www.freebase.com/> .
+@prefix nci: <http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#> .
+@prefix nfo: <http://www.semanticdesktop.org/ontologies/nfo/#> .
+@prefix ng: <http://www.openlinksw.com/schemas/ning#> .
+@prefix nyt: <http://www.nytimes.com/> .
+@prefix oai: <http://www.openarchives.org/OAI/2.0/> .
+@prefix oai_dc: <http://www.openarchives.org/OAI/2.0/oai_dc/> .
+@prefix ogc: <http://www.opengis.net/> .
+@prefix ogcgml: <http://www.opengis.net/ont/gml#> .
+@prefix ogcgs: <http://www.opengis.net/ont/geosparql#> .
+@prefix ogcgsf: <http://www.opengis.net/def/function/geosparql/> .
+@prefix ogcgsr: <http://www.opengis.net/def/rule/geosparql/> .
+@prefix ogcsf: <http://www.opengis.net/ont/sf#> .
+@prefix openSearch: <http://a9.com/-/spec/opensearchrss/1.0/> .
+@prefix opencyc: <http://sw.opencyc.org/2008/06/10/concept/> .
+@prefix opl: <http://www.openlinksw.com/schema/attribution#> .
+@prefix opl-gs: <http://www.openlinksw.com/schemas/getsatisfaction/> .
+@prefix opl-meetup: <http://www.openlinksw.com/schemas/meetup/> .
+@prefix opl-xbrl: <http://www.openlinksw.com/schemas/xbrl/> .
+@prefix oplweb: <http://www.openlinksw.com/schemas/oplweb#> .
+@prefix ore: <http://www.openarchives.org/ore/terms/> .
+@prefix product: <http://www.buy.com/rss/module/productV2/> .
+@prefix protseq: <http://purl.org/science/protein/bysequence/> .
+@prefix r: <http://backend.userland.com/rss2> .
+@prefix radio: <http://www.radiopop.co.uk/> .
+@prefix rev: <http://purl.org/stuff/rev#> .
+@prefix review: <http://www.purl.org/stuff/rev#> .
+@prefix rss: <http://purl.org/rss/1.0/> .
+@prefix sc: <http://purl.org/science/owl/sciencecommons/> .
+@prefix scovo: <http://purl.org/NET/scovo#> .
+@prefix sd: <http://www.w3.org/ns/sparql-service-description#> .
+@prefix sioc: <http://rdfs.org/sioc/ns#> .
+@prefix sioct: <http://rdfs.org/sioc/types#> .
+@prefix skiresort: <http://www.openlinksw.com/ski_resorts/schema#> .
+@prefix skos: <http://www.w3.org/2004/02/skos/core#> .
+@prefix slash: <http://purl.org/rss/1.0/modules/slash/> .
+@prefix stock: <http://xbrlontology.com/ontology/finance/stock_market#> .
+@prefix twfy: <http://www.openlinksw.com/schemas/twfy#> .
+@prefix umbel: <http://umbel.org/umbel#> .
+@prefix umbel-ac: <http://umbel.org/umbel/ac/> .
+@prefix umbel-rc: <http://umbel.org/umbel/rc/> .
+@prefix umbel-sc: <http://umbel.org/umbel/sc/> .
+@prefix uniprot: <http://purl.uniprot.org/> .
+@prefix units: <http://dbpedia.org/units/> .
+@prefix usc: <http://www.rdfabout.com/rdf/schema/uscensus/details/100pct/> .
+@prefix v: <http://www.openlinksw.com/xsltext/> .
+@prefix vcard: <http://www.w3.org/2001/vcard-rdf/3.0#> .
+@prefix vcard2006: <http://www.w3.org/2006/vcard/ns#> .
+@prefix vi: <http://www.openlinksw.com/virtuoso/xslt/> .
+@prefix virt: <http://www.openlinksw.com/virtuoso/xslt> .
+@prefix virtcxml: <http://www.openlinksw.com/schemas/virtcxml#> .
+@prefix virtpivot: <http://www.openlinksw.com/schemas/virtpivot#> .
+@prefix virtrdf: <http://www.openlinksw.com/schemas/virtrdf#> .
+@prefix void: <http://rdfs.org/ns/void#> .
+@prefix wb: <http://www.worldbank.org/> .
+@prefix wdrs: <http://www.w3.org/2007/05/powder-s#> .
+@prefix wf: <http://www.w3.org/2005/01/wf/flow#> .
+@prefix wfw: <http://wellformedweb.org/CommentAPI/> .
+@prefix wikicompany: <http://dbpedia.openlinksw.com/wikicompany/> .
+@prefix wikidata: <http://www.wikidata.org/entity/> .
+@prefix xf: <http://www.w3.org/2004/07/xpath-functions> .
+@prefix xfn: <http://gmpg.org/xfn/11#> .
+@prefix xhtml: <http://www.w3.org/1999/xhtml> .
+@prefix xhv: <http://www.w3.org/1999/xhtml/vocab#> .
+@prefix xi: <http://www.xbrl.org/2003/instance> .
+@prefix xml: <http://www.w3.org/XML/1998/namespace> .
+@prefix xn: <http://www.ning.com/atom/1.0> .
+@prefix xsl10: <http://www.w3.org/XSL/Transform/1.0> .
+@prefix xsl1999: <http://www.w3.org/1999/XSL/Transform> .
+@prefix xslwd: <http://www.w3.org/TR/WD-xsl> .
+@prefix yago: <http://dbpedia.org/class/yago/> .
+@prefix yago-res: <http://mpii.de/yago/resource/> .
+@prefix yt: <http://gdata.youtube.com/schemas/2007> .
+@prefix zem: <http://s.zemanta.com/ns#> .
+' > /dev/null 
+
+sed -i -e 's/^ \(.*\)$/\1/' "$(dirname $OUTPUT)/rascunho1"
+cat "$(dirname $OUTPUT)/rascunho2" "$(dirname $OUTPUT)/rascunho1" > "$OUTPUT"
+
+rm "$(dirname $OUTPUT)/rascunho1"
+rm "$(dirname $OUTPUT)/rascunho2"
 
 exit 0
 
